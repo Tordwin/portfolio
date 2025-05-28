d82a00883532b53ceb6dc672ad100153774e6d01
Edwin Chen <ec7233@g.rit.edu>
2025-05-27 21:26:19 -0400

    Footer DONE

--- footer.php
+++ footer.php
@@ -1,10 +1,15 @@
-        <div class="footerContainer">
-            <div class="icons">
-                <a href="#"><i class="fa-brands fa-facebook"></i></a>
-                <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
-                <a href="#"><i class="fa-brands fa-linkedin"></i></a>
-                <a href="#"><i class="fa-brands fa-github"></i></a>
+        <footer>
+            <div class="footer-container">
+                <div class="icons">
+                    <a href="https://www.facebook.com/chenedwin6/"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/tordwin/"><i class="fa-brands fa-square-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/chenedwin6"><i class="fa-brands fa-linkedin"></i></a>
+                    <a href="https://github.com/Tordwin"><i class="fa-brands fa-github"></i></a>
+                </div>
+                <div class="footer-copyright">
+                    <p>Copyright &copy; 2025; Designed by <span class="designer">Edwin Chen</span></p>
+                </div>
             </div>
-        </div>
+        </footer>
     </body>
 </html>
